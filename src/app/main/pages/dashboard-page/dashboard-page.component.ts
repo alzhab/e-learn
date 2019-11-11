23c35c20bb765b6544b3333ffa9c0a2c6117f7e7
alzhab <abd.alz.b>
2019-11-12 01:05:12 +0600

    карточка с announcement на странице dashboard

--- src/app/main/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/main/pages/dashboard-page/dashboard-page.component.ts
@@ -6,6 +6,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
+  public list = [
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    },
+    {
+      view: 12312,
+      title: 'MERRY CHRISTMAS EVERYONE AND DAYS OFF SCHEDULE',
+      text: `Lorem ipsum dolor sit amet consectetur adi
+      pisicing elit. Animi molestias enim labor
+      iosam nisi fuga modi, doloribus laborum 
+      alias corporis qui non vero fugiat voluptate. Est.`
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
